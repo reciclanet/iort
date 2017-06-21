07ad3f6fa31cf98b3622aa4e9f3cd0e7cf14ffa7
MikelMR <mikelmr@protonmail.com>
2017-06-21 15:42:30 +0200

    Pasar a tablas los datos de index de organizaciones y personas.

--- resources/views/organizaciones/index.blade.php
+++ resources/views/organizaciones/index.blade.php
@@ -4,10 +4,27 @@
   <h1>Organizaciones</h1>
   <a href="{{ url('organizaciones/create')}}" class="btn btn-primary">Nueva</a>
   <div>
-    <ul>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Razón Social</th>
+          <th>Población</th>
+          <th>Teléfono 1</th>
+          <th>Teléfono 2</th>
+        </tr>
+      </thead>
+      <tbody>
+
       @foreach ($organizaciones as $organizacion)
-        <li><a href="/organizaciones/{{ $organizacion->id }}">{{ $organizacion->razon_social }}</a></li>
+        <tr>
+          <th scope="row">{{ $organizacion->id }}</th>
+          <td><a href="/organizaciones/{{ $organizacion->id }}">{{ $organizacion->razon_social }}</a></td>
+          <td>{{ $organizacion->poblacion }}</td>
+          <td>{{ $organizacion->telefono_1 }}</td>
+          <td>{{ $organizacion->telefono_2 }}</td>
+        </tr>
       @endforeach
-    </ul>
+    </table>
   </div>
 @endsection
--- resources/views/personas/index.blade.php
+++ resources/views/personas/index.blade.php
@@ -3,11 +3,34 @@
 @section('content')
   <h1>Personas</h1>
   <a href="{{ url('personas/create')}}" class="btn btn-primary">Nueva</a>
+
   <div>
-    <ul>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nombre</th>
+          <th>Apellido 1</th>
+          <th>Apellido 2</th>
+          <th>Población</th>
+          <th>Teléfono 1</th>
+          <th>Teléfono 2</th>
+        </tr>
+      </thead>
+      <tbody>
+
       @foreach ($personas as $persona)
-        <li><a href="/personas/{{ $persona->id }}">{{ $persona->nombre }}</a></li>
+        <tr>
+          <th scope="row">{{ $persona->id }}</th>
+          <td><a href="/personas/{{ $persona->id }}">{{ $persona->nombre }}</a></td>
+          <td>{{ $persona->apellido_1 }}</td>
+          <td>{{ $persona->apellido_2 }}</td>
+          <td>{{ $persona->poblacion }}</td>
+          <td>{{ $persona->telefono_1 }}</td>
+          <td>{{ $persona->telefono_2 }}</td>
+        </tr>
       @endforeach
-    </ul>
+    </table>
   </div>
+
 @endsection
