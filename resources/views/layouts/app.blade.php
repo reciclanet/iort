9aee4a3a37d2628149f9f25f620d0911a2914b3f
MikelMR <mikelmr@protonmail.com>
2017-08-08 15:19:28 +0200

    Añadir Soporte

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,10 @@
                     <ul class="nav navbar-nav">
                         &nbsp;
                     </ul>
-
+                    <!-- TODO: Borrar esto, es mientras siga el desarrollo -->
+                    <ul class="nav navbar-nav navbar-right">
+                      <a href="mailto:mikelmarcosramos@gmail.com?Subject=[IORT] Incidencia">Contactar</a>
+                    </ul>
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
@@ -55,9 +58,6 @@
                             <li><a href="{{ route('login') }}">Conectarse</a></li>
                             {{-- <li><a href="{{ route('register') }}">Register</a></li> --}}
                         @else
-                            <li>
-                              <a href="personas" aria-expanded="false">Personas</a>
-                            </li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
