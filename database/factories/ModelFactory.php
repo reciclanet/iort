244b8ef5d1cf631af963aed7d99226528ba846d6
MikelMR <mikelmr@protonmail.com>
2017-09-26 16:40:47 +0200

    Rellenar la base de datos con datos para desarrollo. Tablas Persona y Organización.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,19 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+$factory->define(App\Persona::class, function (Faker\Generator $faker) {
+    return [
+        'nombre' => $faker->firstName,
+        'apellido_1' => $faker->lastName,
+        'apellido_2' => $faker->lastName,
+        'email' => $faker->unique()->safeEmail,
+        'fecha_nacimiento' => $faker->date($format = 'Y-m-d', $max = '-18 years'),
+    ];
+});
+$factory->define(App\Organizacion::class, function (Faker\Generator $faker) {
+    return [
+        'razon_social' => $faker->Company,
+        'email' => $faker->unique()->safeEmail,
+        'pagina_web' => $faker->url,
+    ];
+});
