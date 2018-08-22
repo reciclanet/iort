6280124fb364a34df8cb24a6b480a08ef246aa3c
MikelMR <mikelmr@protonmail.com>
2018-08-22 16:01:50 +0200

    Permitir loguear usuarios de forma sencilla.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,24 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\User;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $user;
+
+    public function signIn($user = null)
+    {
+      if (! $user) {
+        $user = factory(User::class)->create();
+      }
+
+      $this->user = $user;
+
+      $this->actingAs($this->user);
+
+      return $this;
+    }
 }
