b9855825d1dd41823dee5af6392d78db18d6bafa
Mikel MR <mikelmr@protonmail.com>
2018-09-08 12:59:18 +0200

    Obligar a estar logueado para marcar los materiales como Txae.

--- app/Http/Controllers/TxaeController.php
+++ app/Http/Controllers/TxaeController.php
@@ -7,35 +7,40 @@ use App\LoteMaterial;
 
 class TxaeController extends Controller
 {
-  public function edit()
-  {
-      return view('txaes.edit');
-  }
-
-  public function update()
-  {
-    $this->validate(request(), [
-      'codigo' => 'required|numeric',
-    ]);
-    $materiales = LoteMaterial::Entrada()
-      ->whereCodigo(request()->input('codigo'))
-      ->where(function ($query) {
-          $query->whereNull('txae')
-                ->orWhere('txae','=','0');
-      })
-      ->get();
-
-    $contador = 0;
-    foreach($materiales as $material) {
-      $material->txae = 1;
-      $material->save();
-      $contador++;
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
 
-    $mensaje = "Se han marcado $contador registros.";
+    public function edit()
+    {
+        return view('txaes.edit');
+    }
 
-    return redirect()
-      ->action('TxaeController@edit')
-      ->with('message', $mensaje);
-  }
+    public function update()
+    {
+        $this->validate(request(), [
+          'codigo' => 'required|numeric',
+        ]);
+        $materiales = LoteMaterial::Entrada()
+          ->whereCodigo(request()->input('codigo'))
+          ->where(function ($query) {
+              $query->whereNull('txae')
+                    ->orWhere('txae','=','0');
+          })
+          ->get();
+
+        $contador = 0;
+        foreach($materiales as $material) {
+          $material->txae = 1;
+          $material->save();
+          $contador++;
+        }
+
+        $mensaje = "Se han marcado $contador registros.";
+
+        return redirect()
+          ->action('TxaeController@edit')
+          ->with('message', $mensaje);
+    }
 }
